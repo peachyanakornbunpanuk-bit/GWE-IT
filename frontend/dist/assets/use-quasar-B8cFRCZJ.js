62222d1778b60b2f5700da6f5a7631d29e79ca4a
Yanakorn Banphanuk <65160159@go.buu.ac.th>
2026-07-16 09:24:06 +0700

    fix: update API_URL to relative path for unified deployment

--- frontend/dist/assets/use-quasar-B8cFRCZJ.js
+++
@@ -1,1 +0,0 @@
-import{a8 as a,aU as r}from"./index-BKUHXqjj.js";function u(){return a(r)}export{u};
---
+++ frontend/dist/assets/use-quasar-C7Wm2W3f.js
@@ -0,0 +1,1 @@
+import{a8 as a,aU as r}from"./index-Cf5ltvUk.js";function u(){return a(r)}export{u};
