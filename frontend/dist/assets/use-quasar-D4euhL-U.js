62a8d669847cd9c11acd1bb3b57fc3a1cf7a83ec
Yanakorn Banphanuk <65160159@go.buu.ac.th>
2026-07-16 14:26:52 +0700

    style: remove default login accounts text, apply global earthy quasar palette, and implement minimalist table headers

---
+++ frontend/dist/assets/use-quasar-D4euhL-U.js
@@ -0,0 +1,1 @@
+import{ac as a,aX as r}from"./index-_7Gesvqy.js";function u(){return a(r)}export{u};
--- frontend/dist/assets/use-quasar-mZdMb3x0.js
+++
@@ -1,1 +0,0 @@
-import{ac as a,aX as r}from"./index-BFjlkPST.js";function u(){return a(r)}export{u};
