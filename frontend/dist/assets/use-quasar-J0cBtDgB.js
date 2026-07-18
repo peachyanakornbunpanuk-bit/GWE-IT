f8a3a5bbd1fceb7d5db20e0aa20a9e4da603987b
Yanakorn Banphanuk <65160159@go.buu.ac.th>
2026-07-18 14:07:16 +0700

    fix: production API URL

--- frontend/dist/assets/use-quasar-J0cBtDgB.js
+++
@@ -1,1 +0,0 @@
-import{ad as a,aY as r}from"./index-B0wKDL5E.js";function u(){return a(r)}export{u};
---
+++ frontend/dist/assets/use-quasar-dIXa7w0S.js
@@ -0,0 +1,1 @@
+import{ad as a,aY as r}from"./index-BAiK1_ll.js";function u(){return a(r)}export{u};
