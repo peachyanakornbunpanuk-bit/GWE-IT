159d4cf89dc2739f650ac8aeec53709bde915795
Yanakorn Banphanuk <65160159@go.buu.ac.th>
2026-07-23 10:57:03 +0700

    Fix print scoped CSS trap in Asset.vue

--- frontend/dist/assets/use-quasar-B4lQnJTL.js
+++
@@ -1,1 +0,0 @@
-import{ad as a,aY as r}from"./index-OU78W6j4.js";function u(){return a(r)}export{u};
---
+++ frontend/dist/assets/use-quasar-CJ5D_qrs.js
@@ -0,0 +1,1 @@
+import{ad as a,aY as r}from"./index-9GEfMhqd.js";function u(){return a(r)}export{u};
